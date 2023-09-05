4715d947b6d4ff39f701685a3dd360706a08ff2d
JhonatanVeliz <jhonatanveliz74@gmail.com>
2023-09-05 17:46:58 -0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from "./Layout";
+
+const App = () => {
+    
+  return (
+    <div className='container'>
+        <Layout />
+    </div>
+  )
+}
+
+export default App
